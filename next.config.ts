54aecfc7d3572b7e1ed6198a8fccdba7533d7399
lucasmsqt <lucacsk3@gmail.com>
2024-12-28 17:00:53 -0300

    feat: configura next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,20 @@
 import type { NextConfig } from "next";
+import dotenv from 'dotenv'
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/auth/:path*',
+        destination: `${process.env.API_URL}/auth/:path*`
+      },
+      {
+        source: '/contas/:path*',
+        destination: `${process.env.API_URL}/contas/:path*`
+      },
+    ];
+  },
+}
 
 const nextConfig: NextConfig = {
   /* config options here */
